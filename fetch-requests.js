6f80213c7b1fe56a029898812298e7d9a74cbe47
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2022-08-10 20:48:44 -0700

    phase 1

--- fetch-requests.js
+++ fetch-requests.js
@@ -6,9 +6,32 @@ Paste your code for fetch requests here once you finish each phase.
 /* ============================== Phase 1 ============================== */
 
 // Your code here
+fetch('/products', {
+    method: "POST",
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+})
 
+async function first() {
+    try {
+        const url = '/products'
+        const options = {
+            method: "POST",
+            body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+            headers: {
+                 "Content-Type": "application/x-www-form-urlencoded"
+            }}
+        const resObj = await fetch(url, options)
+        return resObj
 
+    } catch (error) {
+        console.log('Error: ', error)
+    }
+}
 
+first()
 /* ============================== Phase 2 ============================== */
 
 // Your code here
