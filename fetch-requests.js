d8964e41d188b4c5e28191655fe7d8b70bf1214a
erenali22 <erena.li@outlook.com>
2022-08-10 21:06:36 -0700

    phase 2

--- fetch-requests.js
+++ fetch-requests.js
@@ -6,37 +6,56 @@ Paste your code for fetch requests here once you finish each phase.
 /* ============================== Phase 1 ============================== */
 
 // Your code here
-fetch('/products', {
-    method: "POST",
-    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-})
+fetch("/products", {
+  method: "POST",
+  body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+});
 
 async function first() {
-    try {
-        const url = '/products'
-        const options = {
-            method: "POST",
-            body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
-            headers: {
-                 "Content-Type": "application/x-www-form-urlencoded"
-            }}
-        const resObj = await fetch(url, options)
-        return resObj
-
-    } catch (error) {
-        console.log('Error: ', error)
-    }
+  try {
+    const url = "/products";
+    const options = {
+      method: "POST",
+      body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    };
+    const resObj = await fetch(url, options);
+    return resObj;
+  } catch (error) {
+    console.log("Error: ", error);
+  }
 }
 
-first()
+first();
 /* ============================== Phase 2 ============================== */
 
-// Your code here
-
-
+async function second() {
+  try {
+    const data = await fetch("/products", {
+      method: "POST",
+      body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    const print = {
+      statusCode: data.status,
+      contentType: data.headers.get("Content-Type"),
+      redirected: data.redirected,
+      url: data.url,
+    };
+    console.log(print);
+    return print;
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+}
+second();
 
 /* ============================== Phase 3 ============================== */
 
